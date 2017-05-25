960ad5eb9a0be999390b5601c39741ee0bdea147
Rowen Scout <rowenscout@gmail.com>
2017-05-24 21:33:53 -0700

    initial commit with project scaff

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var confirmed = confirm('Are you ready to rumble?');
+
+if (confirmed === true){
+  console.log('The user is rumblin\'');
+} else {
+  console.log('User is not rumblin\' but too bad, we\'re doing it anyway. \:\)');
+}
