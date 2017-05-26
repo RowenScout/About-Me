d0d8e04fbb11ea56a9af1e30ffa0b7c1b5b8dd86
Rowen Scout <rowenscout@gmail.com>
2017-05-25 19:14:54 -0700

    Added framework for prompt questions and if/else control flow.

--- app.js
+++ app.js
@@ -7,3 +7,38 @@ if (confirmed === true){
 } else {
   console.log('User is not rumblin\' but too bad, we\'re doing it anyway. \:\)');
 }
+var name = prompt("text");
+if (name.toUpper === 'Y') {
+  console.log('User\'s choice was ' + name + '.'
+  alert('Correct, next round.')}
+  else {
+    console.log('User\'s input was incorrect or invalid.')
+    alert('Wrong! Next question'}
+var name = prompt("text");
+if (name.toUpper === 'Y') {
+  console.log('User\'s choice was ' + name + '.' 
+  alert('Correct, next round.')}
+  else {
+    console.log('User\'s input was incorrect or invalid.')
+    alert('Wrong! Next question'}
+var name = prompt("text");
+if (name.toUpper === 'Y') {
+  console.log('User\'s choice was ' + name + '.'
+  alert('Correct, next round.')}
+  else {
+    console.log('User\'s input was incorrect or invalid.')
+    alert('Wrong! Next question'}
+var name = prompt("text");
+if (name.toUpper === 'Y') {
+  console.log('User\'s choice was ' + name + '.'
+  alert('Correct, next round.')}
+  else {
+    console.log('User\'s input was incorrect or invalid.')
+    alert('Wrong! Next question'}
+var name = prompt("text");
+if (name.toUpper === 'Y') {
+  console.log('User\'s choice was ' + name + '.'
+  alert('Correct, next round.')}
+  else {
+    console.log('User\'s input was incorrect or invalid.')
+    alert('Wrong! Next question'}
